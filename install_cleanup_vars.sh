e59f774de4230d3fdca8779707d094b1d7f9cfef
Kenichiro Komazawa <kkomazaw@gmail.com>
2023-03-14 21:06:14 +0900

    Added images for Toyota

--- install_cleanup_vars.sh
+++ install_cleanup_vars.sh
@@ -1,16 +1,16 @@
 # The namespace to deploy the environment in
-NAMESPACE=
+NAMESPACE=bobbycar
 # The clusters app domain without port
-APP_DOMAIN=
+APP_DOMAIN=apps.cluster-mhkcf.mhkcf.sandbox129.opentlc.com
 # The clusters api domain without port
-API_DOMAIN=
+API_DOMAIN=api.cluster-mhkcf.mhkcf.sandbox129.opentlc.com
 
 # Google Maps API key - https://developers.google.com/maps/documentation/javascript/get-api-key
-GOOGLE_API_KEY=
+GOOGLE_API_KEY=AIzaSyArNeEf3pS9UgmrMK-f9b0omUAjecK5Tdk
 # OpenWeatherMap API Key - https://openweathermap.org/api
-OWM_WEATHER_API_KEY=
+OWM_WEATHER_API_KEY=bobbycar
 # https://www.ibm.com/products/environmental-intelligence-suite/data-packages
-IBM_WEATHER_API_KEY=
+IBM_WEATHER_API_KEY=bobbycar
 
 # Installs OpenShift Serverless in the cluster
 INSTALL_KNATIVE=true
